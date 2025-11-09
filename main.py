7f33434647bccbfa2955d959c7eb9391200152ce
Aadil-star <92925311+Aadil-star@users.noreply.github.com>
2025-11-09 19:58:04 +0530

    main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import argparse
 from datetime import datetime
 import requests
 from mailjet_rest import Client
+from config import MAILJET_API_KEY, MAILJET_SECRET_KEY, USER_EMAIL
+
 
 # --------- Configuration (via environment variables) ----------
 # Required Mailjet env vars:
